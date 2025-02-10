184e8f8d4b2a89d241231ba9d4e0205f28406c69
arshiaTests <arshia.org79@gmail.com>
2025-02-10 14:11:25 +0330

    array method

---
+++ ex.1.js
@@ -0,0 +1,14 @@
+//تمرین 1: پیدا کردن میانگین نمرات با reduce
+//لیستی از نمرات داریم:
+
+//const scores = [15, 18, 12, 20, 10, 14, 16];
+
+//با استفاده از reduce، میانگین این نمرات را محاسبه کن.
+function average (num){
+    let sum = 0;
+    for (let i = 0 ;i<num.length ; i++){
+        sum+= num[i]
+    }
+    return sum / num.length
+}
+console.log(average([1,2,3,4]))
