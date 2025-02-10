4d728c6f368597c01cf541b25e3d30db5fdc9a90
arshiaTests <arshia.org79@gmail.com>
2025-02-10 14:25:24 +0330

    filter num under 10

---
+++ ex2.js
@@ -0,0 +1,13 @@
+//تمرین 2: فیلتر کردن نمرات زیر 10 با filter
+//لیست نمرات را به نحوی فیلتر کن که فقط مقادیری که کمتر از 10 هستند باقی بمانند.
+
+function find (score){
+    let filter =[]
+    for(let i = 0 ; i<score.length;i++){
+        if(score[i]<10){
+            filter.push(score[i])
+        }
+    }
+    return filter
+}
+console.log(find([1,2,3,4,5,567,73,44,23,2,35,]))
