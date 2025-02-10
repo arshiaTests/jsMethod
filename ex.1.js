413200035ce6fa5aa8c16df74012a30d62d413a2
arshiaTests <arshia.org79@gmail.com>
2025-02-10 14:17:04 +0330

    with reduce

--- ex.1.js
+++ ex.1.js
@@ -4,11 +4,8 @@
 //const scores = [15, 18, 12, 20, 10, 14, 16];
 
 //با استفاده از reduce، میانگین این نمرات را محاسبه کن.
-function average (num){
-    let sum = 0;
-    for (let i = 0 ;i<num.length ; i++){
-        sum+= num[i]
-    }
-    return sum / num.length
+function average(num){
+    let sum = num.reduce((a,b)=>a+b,0);
+    return sum / num.length;
 }
 console.log(average([1,2,3,4]))
