2be6cb62ed0133f80f256c8269a11e2b36ff6290
arshiaTests <arshia.org79@gmail.com>
2025-02-10 23:03:38 +0330

    is it + or not

---
+++ ex6.js
@@ -0,0 +1,10 @@
+//بررسی اینکه همه عناصر بزرگ‌تر از صفر هستند یا نه با every
+function all (arr){
+    for (let i = 0 ; i<arr.length;i++){
+        if(arr[i]<=0){
+            return false
+        }
+    }
+    return true
+}
+console.log(all([1,2,3,4,12,3,32,-4,3]))
