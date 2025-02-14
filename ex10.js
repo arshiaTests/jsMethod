8de3ddf7d090970e4807c4e1c3dbd1152f776162
arshiaTests <arshia.org79@gmail.com>
2025-02-14 20:42:41 +0330

    to green

---
+++ ex10.js
@@ -0,0 +1,13 @@
+function findIndex(arr, target) {
+    let index = -1;
+    arr.some((num, i) => {
+        if (num === target) {
+            index = i;
+            return true; // متوقف کردن عملیات بعد از پیدا شدن مقدار
+        }
+        return false;
+    });
+    return index !== -1 ? `ایندکس: ${index}` : "پیدا نشد";
+}
+
+console.log(findIndex([10, 20, 34, 2, 233], 20)); // ایندکس: 1
