b2917814248f243200690f222025fb7948f1488d
arshiaTests <arshia.org79@gmail.com>
2025-02-14 20:48:01 +0330

    for update

--- ex11.js
+++ ex11.js
@@ -11,3 +11,10 @@ function findIndex(arr, target) {
 }
 
 console.log(findIndex([10, 20, 34, 2, 233], 20)); // ایندکس: 1
+function findIndex(arr, target) {
+    let index = arr.findIndex(num => num === target);
+    return index !== -1 ? `ایندکس: ${index}` : "پیدا نشد";
+}
+
+console.log(findIndex([10, 20, 34, 2, 233], 20)); // ایندکس: 1
+
