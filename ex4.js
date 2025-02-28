63b1f9c8f8537ac004b9848c35151987f0f4376b
arshiaTests <arshia.org79@gmail.com>
2025-02-28 20:21:50 +0330

    update ex4

--- ex4.js
+++ ex4.js
@@ -10,3 +10,11 @@ function find (arr){
 
 }  
 console.log(find([1,2,3,4,5,6,]))
+
+const findFirstNumberGreaterThan15 = (arr) => {
+    const result = arr.find((num) => num > 15);
+    return result !== undefined ? result : "عددی پیدا نشد";
+  };
+  
+  console.log("اولین عدد بزرگ‌تر از ۱۵:", findFirstNumberGreaterThan15([1, 2, 3, 4, 5, 6])); // خروجی: "عددی پیدا نشد"
+  console.log("اولین عدد بزرگ‌تر از ۱۵:", findFirstNumberGreaterThan15([10, 20, 30, 40])); // خروجی: 20
