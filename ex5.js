d8c9b9f02d31f0f42fbca6dc503b67aa49f70174
arshiaTests <arshia.org79@gmail.com>
2025-02-28 20:22:45 +0330

    update ex5

--- ex5.js
+++ ex5.js
@@ -8,3 +8,10 @@ function neg (arr){
 }
 console.log(neg([1,2,3,45,32,-3,5,3,32,4,23,3]))
 //تمرین پنجم: بررسی وجود حداقل یک عدد منفی
+
+const hasNegativeNumber = (arr) => {
+    return arr.some((num) => num < 0);
+  };
+  
+  console.log("آیا عدد منفی وجود دارد؟", hasNegativeNumber([1, 2, 3, 45, 32, -3, 5, 3, 32, 4, 23, 3])); // خروجی: true
+  console.log("آیا عدد منفی وجود دارد؟", hasNegativeNumber([1, 2, 3, 4, 5])); // خروجی: false
