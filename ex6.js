6d412c2204e6e9c833c9184d2bd227180914acfa
arshiaTests <arshia.org79@gmail.com>
2025-03-01 00:53:16 +0330

    update

--- ex6.js
+++ ex6.js
@@ -1,10 +1,26 @@
-//بررسی اینکه همه عناصر بزرگ‌تر از صفر هستند یا نه با every
-function all (arr){
-    for (let i = 0 ; i<arr.length;i++){
-        if(arr[i]<=0){
-            return arr[i]
-        }
-    }
-    return true
-}
-console.log(all([1,2,3,4,12,3,32,-4,3]))
+const allPositiveNumbers = (arr) => {
+    return arr.every((num) => num > 0);
+  };
+  
+  console.log("آیا همه اعداد مثبت هستند؟", allPositiveNumbers([1, 2, 3, 4, 12, 3, 32, -4, 3])); // خروجی: false
+  console.log("آیا همه اعداد مثبت هستند؟", allPositiveNumbers([1, 2, 3, 4, 12, 3, 32, 3])); // خروجی: true
+  //بررسی اینکه همه‌ی اعداد زوج هستند
+  const allEvenNumbers = (arr) => {
+    return arr.every((num) => num % 2 === 0);
+  };
+  
+  console.log("آیا همه اعداد زوج هستند؟", allEvenNumbers([2, 4, 6, 8])); // خروجی: true
+  console.log("آیا همه اعداد زوج هستند؟", allEvenNumbers([2, 4, 6, 7])); // خروجی: false
+
+  // بررسی اینکه همه‌ی کاربران بالای ۱۸ سال هستند
+  const users = [
+    { name: "آرشیا", age: 25 },
+    { name: "علی", age: 17 },
+    { name: "مریم", age: 20 },
+  ];
+  
+  const allAdults = (arr) => {
+    return arr.every((user) => user.age > 18);
+  };
+  
+  console.log("آیا همه کاربران بالای ۱۸ سال هستند؟", allAdults(users)); // خروجی: false
