4981f4f369d5e9b52c5c2f944778b39c22109c28
arshiaTests <arshia.org79@gmail.com>
2025-02-28 20:20:36 +0330

    update ex3

--- ex3.js
+++ ex3.js
@@ -7,3 +7,9 @@ function lvlUp (array){
     return updateArray
 }
 console.log(lvlUp([1,2,3,4,5,6,7,8]))
+
+const lvlUpp = (array) => {
+    return array.map((score) => score + 5);
+  };
+  
+  console.log("نمرات افزایش یافته:", lvlUpp([1, 2, 3, 4, 5, 6, 7, 8])); // خروجی: [6, 7, 8, 9, 10, 11, 12, 13]
