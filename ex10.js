0367d30ed59929966dc8c809da29a02c2621e3cc
arshiaTests <arshia.org79@gmail.com>
2025-03-05 17:32:38 +0330

    update num 10

--- ex10.js
+++ ex10.js
@@ -8,3 +8,17 @@ function removeDuplicates(arr) {
     return uniqueArray;
   }
   console.log(removeDuplicates([1,1,1,2,2,,3,4,,54,2,23,3]))
+
+  const removeDuplicatesPreserveOrder = (arr) => {
+    const seen = new Set();
+    return arr.filter((num) => {
+      if (!seen.has(num)) {
+        seen.add(num);
+        return true;
+      }
+      return false;
+    });
+  };
+  
+  console.log(removeDuplicatesPreserveOrder([1, 2, 2, 3, 4, 4, 5])); // خروجی: [1, 2, 3, 4, 5]
+  
