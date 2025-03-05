d66ae544a792393da9be7d45f652f91e6c61ba0c
arshiaTests <arshia.org79@gmail.com>
2025-03-05 22:49:40 +0330

    update 12

--- ex12.js
+++ ex12.js
@@ -14,3 +14,26 @@ console.log(secondLargest([10, 20, 5, 8, 30])); // 20
 console.log(secondLargest([3, 7, 3, 7, 9])); // 7
 console.log(secondLargest([5, 5, 5])); // "عدد دوم بزرگ‌تر وجود ندارد"
 console.log(secondLargest([100])); // "عدد دوم بزرگ‌تر وجود ندارد"
+
+//پیدا کردن دومین عدد کوچک‌تر
+const secondSmallest = (arr) => {
+    if (arr.length < 2) return "عدد دوم کوچک‌تر وجود ندارد";
+  
+    let min = Infinity;
+    let secondMin = Infinity;
+  
+    for (let num of arr) {
+      if (num < min) {
+        secondMin = min;
+        min = num;
+      } else if (num < secondMin && num !== min) {
+        secondMin = num;
+      }
+    }
+  
+    return secondMin !== Infinity ? secondMin : "عدد دوم کوچک‌تر وجود ندارد";
+  };
+  
+  console.log(secondSmallest([10, 20, 5, 8, 30])); // 8
+  console.log(secondSmallest([3, 7, 3, 7, 9])); // 7
+  console.log(secondSmallest([5, 5, 5])); // "عدد دوم کوچک‌تر وجود ندارد"
