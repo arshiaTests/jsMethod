5af2b53f14d8a057eaf2a1ee813c476b6a3b2e2f
arshiaTests <arshia.org79@gmail.com>
2025-03-05 22:21:46 +0330

    update num 11

--- ex11.js
+++ ex11.js
@@ -18,3 +18,14 @@ function findIndex(arr, target) {
 
 console.log(findIndex([10, 20, 34, 2, 233], 20)); // ایندکس: 1
 
+
+//پیدا کردن همه‌ی ایندکس‌های یک عدد در آرایه
+const findAllIndices = (arr, target) => {
+    const indices = [];
+    arr.forEach((num, i) => {
+      if (num === target) indices.push(i);
+    });
+    return indices.length > 0 ? `ایندکس‌ها: ${indices.join(", ")}` : "پیدا نشد";
+  };
+  
+  console.log(findAllIndices([10, 20, 34, 20, 233], 20)); // خروجی: "ایندکس‌ها: 1, 3"
