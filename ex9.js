9f897aae06e99c8f6d8fb6246c947184380c624f
arshiaTests <arshia.org79@gmail.com>
2025-03-05 17:10:11 +0330

    update num 9

--- ex9.js
+++ ex9.js
@@ -6,3 +6,22 @@ function include (arr,target){
 }
 console.log(include([10,20,34,2,233],20))
 
+const findLastNumberIndex = (arr, target) => {
+    const index = arr.lastIndexOf(target);
+    return index !== -1 ? `${target} در آرایه وجود دارد و آخرین موقعیت آن ${index} است.` : `${target} پیدا نشد.`;
+  };
+  
+  console.log(findLastNumberIndex([10, 20, 34, 20, 233], 20)); // خروجی: "20 در آرایه وجود دارد و آخرین موقعیت آن 3 است."
+
+  // پیدا کردن همه‌ی موقعیت‌های یک عدد در آرایه
+  const findAllNumberIndices = (arr, target) => {
+    const indices = [];
+    arr.forEach((num, index) => {
+      if (num === target) indices.push(index);
+    });
+    return indices.length > 0
+      ? `${target} در آرایه وجود دارد و موقعیت‌های آن ${indices.join(", ")} هستند.`
+      : `${target} پیدا نشد.`;
+  };
+  
+  console.log(findAllNumberIndices([10, 20, 34, 20, 233], 20)); // خروجی: "20 در آرایه وجود دارد و موقعیت‌های آن 1, 3 هستند."
