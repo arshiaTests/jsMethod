1ce81866ef6015fc9bf362c05704fb451abbe6f1
arshiaTests <arshia.org79@gmail.com>
2025-03-10 19:48:40 +0330

    update num 5

--- ex5.js
+++ ex5.js
@@ -15,3 +15,30 @@ const hasNegativeNumber = (arr) => {
   
   console.log("آیا عدد منفی وجود دارد؟", hasNegativeNumber([1, 2, 3, 45, 32, -3, 5, 3, 32, 4, 23, 3])); // خروجی: true
   console.log("آیا عدد منفی وجود دارد؟", hasNegativeNumber([1, 2, 3, 4, 5])); // خروجی: false
+
+//بدیل اعداد به رشته
+  const numbers = [1, 2, 3, 4, 5, 6];
+  const stringNumbers = numbers.map((num) => `عدد: ${num}`);
+  console.log(stringNumbers); // خروجی: ["عدد: 1", "عدد: 2", "عدد: 3", "عدد: 4", "عدد: 5", "عدد: 6"]
+
+// فیلتر کردن و سپس تبدیل
+
+const numberss = [1,2,3,4,5];
+const doubled = numberss.filter((num)=>num%2 ===0).map((num)=>num*2);
+console.log(doubled)
+// تبدیل اعداد به شیء
+const numbersss = [1, 2, 3, 4, 5, 6];
+const numberObjects = numbersss.map((num) => ({
+  value: num,
+  isEven: num % 2 === 0,
+}));
+console.log(numberObjects);
+// خروجی:
+// [
+//   { value: 1, isEven: false },
+//   { value: 2, isEven: true },
+//   { value: 3, isEven: false },
+//   { value: 4, isEven: true },
+//   { value: 5, isEven: false },
+//   { value: 6, isEven: true }
+// ]
