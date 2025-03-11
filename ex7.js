6c5228bb7e8b783ed0f71046a57a5c611843f924
arshiaTests <arshia.org79@gmail.com>
2025-03-11 18:15:43 +0330

    update num7

--- ex7.js
+++ ex7.js
@@ -15,3 +15,22 @@ function sort (arr){
     return arr.sort();
 }
 console.log(sort(["banana","apple","cherry"]))
+
+// مرتب‌سازی اشیاء بر اساس خاصیت
+const sortByAge = (arr) => {
+    return arr.slice().sort((a, b) => a.age - b.age);
+  };
+  
+  const users = [
+    { name: "عرشیا", age: 25 },
+    { name: "علی", age: 17 },
+    { name: "مریم", age: 20 },
+  ];
+  
+  console.log(sortByAge(users));
+  // خروجی:
+  // [
+  //   { name: "علی", age: 17 },
+  //   { name: "مریم", age: 20 },
+  //   { name: "آرشیا", age: 25 }
+  // ]
