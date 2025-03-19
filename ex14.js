492c2ed0401048f60a794d4d895d2b63b2212bca
arshiaTests <arshia.org79@gmail.com>
2025-03-19 19:23:55 +0330

    update num 14

--- ex14.js
+++ ex14.js
@@ -3,3 +3,6 @@ const upperText = text.toUpperCase();
 console.log(upperText);
 //تمرین 1: تبدیل رشته به حروف کوچک
 
+const textt = "HELLO"
+const lower = textt.toLocaleLowerCase();
+console.log(lower)
